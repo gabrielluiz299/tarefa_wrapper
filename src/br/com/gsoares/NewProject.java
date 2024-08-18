0fa3ae2d6f279e88a91e9f50245574f171ed48b6
gabrielluiz299 <gabrielluiz299@gmail.com>
2024-08-17 21:57:06 -0300

    converção dp tipo primitivo para o tipo wrapper

---
+++ src/br/com/gsoares/NewProject.java
@@ -0,0 +1,13 @@
+package br.com.gsoares;
+
+public class NewProject {
+
+	public static void main(String[] args) {
+		int primitivo = 10;
+		System.out.print(primitivo);
+		
+		Integer wrappers = primitivo;
+		System.out.print(wrappers);
+	}
+
+}
